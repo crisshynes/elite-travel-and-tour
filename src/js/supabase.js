d077de68c15ac77ad0e5cb749fa7ca5ef298a335
crisshynes <crisshynes@gmail.com>
2025-12-23 00:05:27 +0000

    supabase.js

--- src/js/supabase.js
+++ src/js/supabase.js
@@ -1,6 +1,6 @@
 // public/js/supabase.js
 import { createClient } from '@supabase/supabase-js';
-import { CFG } from './config.js';
+import { CFG } from './src/js/config.js';
 
 export const sb = createClient(CFG.SUPABASE_URL, CFG.SUPABASE_ANON_KEY, {
   auth: {
